273910fa20f398fd0060d88edf03bd683a6ae907
Alex Hallgren <alexyurihallgren@hotmail.com>
2024-10-28 14:40:47 +0100

    Added some styling, created a footer and did some tests.

--- src/assets/components/Layout/HeaderLayout.tsx
+++ src/assets/components/Layout/HeaderLayout.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom";
+import styles from "./HeaderLayout.module.css";
+import css from "../global-css/master.css"
 
 const HeaderLayout = () => {
   return (
     <>
       <div>
-        <h2>Hej</h2>
+        <footer className={styles.Footer}>
+            <h1>Hej jag är en footer</h1>
+        </footer>
       </div>
       <main>
         <Outlet />
